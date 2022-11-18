e2f1bdcf938ca2b5179e4c5f12b1b61b62f739e0
gomezclaudio11 <claudiogomez23@gmail.com>
2022-11-18 19:08:23 -0300

    desafio 5

--- desafio5/server.js
+++ desafio5/server.js
@@ -14,20 +14,21 @@ const productContenedor = new ProductContenedor();
 const PORT = 8080;
 app.listen(PORT, () => console.log(`Servidor iniciado en el puerto ${PORT}`));
 
+/*
 app.get('/', (req, res) => {
    // ProductContenedor.save(req.body);
     const personList = [];
   res.render('pages/index', { list: personList });
 });
-
+*/
 app.get('/products', (req, res) => {
-    const productsList = productContenedor.getAll()
-    res.render('pages/products', {list: productsList});
-  });
+  const productos = productContenedor.getAll()
+  res.render('pages/products', {list: productos});
+});
 
 app.post('/products', (req, res) => {
     console.log(req.body);
     productContenedor.save(req.body);
     
-    res.redirect('/products'); // TODO enviar datos del historial
+    res.redirect('/products'); 
   });
