2d4f41cb1cbec082b45310ba66edc32f09394c77
gomezclaudio11 <claudiogomez23@gmail.com>
2023-01-05 18:53:05 -0300

    desafio 9

--- desafio9Clase22/database/createTables.js
+++ desafio9Clase22/database/createTables.js
@@ -1,19 +1,6 @@
-/*
 const knex = require('knex');
-
-//mi sql conection + create tables
-const mysqlConnection = {
-    client: 'mysql',
-    connection: {
-      host : '127.0.0.1',
-      port : 3306,
-      user : 'root',
-      password : '',
-      database : 'ecommerce'
-    }
-  };
-  
-  module.exports = mysqlConnection;
+const mysqlConnection = require('./mysqlConnection');
+const sqliteConnection = require('./sqliteConnection');
 
 const createProductTable = async () => {
   try {
@@ -35,14 +22,14 @@ const createProductTable = async () => {
 
 const createMessageTable = async () => {
   try {
-    const database = knex(mysqlConnection);
+    const database = knex(sqliteConnection);
     await database.schema.dropTableIfExists('mensajes');
   
     await database.schema.createTable('mensajes', (table) => {
       table.increments('id').primary()
       table.string('email', 50).notNullable()
       table.string('text', 255)
-      table.timestamps()
+      table.timestamps('date')
     });
   
     console.log('TABLE mensajes created');
@@ -55,18 +42,5 @@ const createTables = async () => {
   await createProductTable();
   await createMessageTable();
 }
-createTables();
-*/
 
-//mongoose
-export default {
-  mongodb: {
-      url: 'mongodb://localhost/ecommerce',
-      options: {
-          //useNewUrlParser: true,
-          //useUnifiedTopology: true,
-          //useCreateIndex: true,
-          serverSelectionTimeoutMS: 5000,
-      }
-  }
-}
+createTables();
---
+++ desafio9Clase22/server.js
@@ -0,0 +1,101 @@
+import express from 'express'
+const { Server: HttpServer } = require('http')
+//const { Server: IOServer } = require('socket.io')
+const productRouter = require("./src/routers/products")
+const messagesRouter = require("./src/routers/messages")
+
+const app = express()
+const httpServer = new HttpServer(app)
+
+/*
+const io = new IOServer(httpServer)
+io.on('connection', (socket) => {
+    console.log('socket id: ', socket.id);
+  
+    socket.emit('products', productContenedorSQL.getAll());
+    
+    socket.emit('conversation', messages);
+    socket.on('new-message', (newMessage) => {
+    console.log({newMessage});
+    messages.push(newMessage);
+    io.sockets.emit('conversation', messages);
+    });
+  });
+
+  const messages = [
+    { mail: 'pepe@gmail.com', text: 'Hola'},
+    { mail: 'grillo@gmail.com', text: 'todo bien?'},
+    { mail: 'pepe@gmail.com', text: 'Si, todo bien!'}
+  ]
+*/
+
+app.use (express.urlencoded({ extended: true}));
+
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+
+const ProductContenedor = require("./src/contenedores/ProductContenedor");
+const productContenedor = new ProductContenedor();
+
+const mysqlConnection = require('./database/mysqlConnection');
+const ProductContenedorSQL = require ("./src/contenedores/ProductContenedorSQL");
+const productContenedorSQL = new ProductContenedorSQL(mysqlConnection, 'productos');
+
+const PORT = 8080;
+httpServer.listen(PORT, () => console.log(`Servidor iniciado en el puerto ${PORT}`));
+
+
+app.get('/', (req, res) => {
+   // ProductContenedor.save(req.body);
+    const personList = [];
+  res.render('pages/index', { list: personList });
+});
+
+app.post('/products', (req, res) => {
+    console.log(req.body);
+    productContenedor.save(req.body);
+    //io.sockets.emit("products", productContenedorSQL.getAll())
+    res.redirect('/'); 
+  }); 
+
+app.get('/products', (req, res) => {
+    console.log(req.body)
+    productContenedorSQL.getAll()
+    res.json();
+});
+
+//fackers
+import { faker } from '@faker-js/faker'
+faker.locale = "ar"
+
+let id = 1
+function getNextId() {
+    return id++
+}
+function crearProductoAlAzar (id){
+  return{
+    id,
+    nombre: faker.commerce.product(),
+    precio: faker.commerce.price(1000, 1000, 0, '$'),
+    fotoUrl: faker.image.food(1234, 2345, true)
+  }
+}
+
+function generarNProductos(cant) {
+  const personas = []
+  for (let i = 0; i < cant; i++) {
+      personas.push(crearProductoAlAzar(getNextId()))
+  }
+  return personas
+}
+
+const CANT_PERS_DEFAULT = 5
+
+app.get('/api/productos-test', (req, res) => {
+  const cant = Number(req.query.cant) || CANT_PERS_DEFAULT
+  res.json(generarNProductos(cant))
+})
+
+app.use("/api/products", productRouter);
+app.use("/api/messages", messagesRouter);
